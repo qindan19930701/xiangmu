31f99dd2b73322ba9882dedc211d023e8ea596c5
qindan19930701 <837096569@qq.com>
2019-11-15 11:03:43 +0800

    引入登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    login--------
+    <el-form label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="success">登录</el-button>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 <style>
-
 </style>
