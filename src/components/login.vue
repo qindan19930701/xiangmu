85cdb6af3beea7d1129f74e7091a12e580d1440b
qindan19930701 <837096569@qq.com>
2019-11-17 10:08:41 +0800

    渲染登录组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-form label-position="top" label-width="80px" :model="formdata">
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
@@ -8,7 +8,7 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button type="success">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="success">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -21,8 +21,51 @@ export default {
         password: ''
       }
     }
+  },
+  methods: {
+    handleLogin () {
+      this.$http
+        .post('login', this.formdata)
+        .then(res => {
+          const {
+            data: {
+              data,
+              meta: { msg, status }
+            }
+
+          } = res
+          console.log(data)
+          if (status === 200) {
+            this.$router.push({
+              name: 'home'
+            })
+          } else {
+            // 提示框
+            this.$message.error(msg)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
 <style>
+.login-wrap {
+  height: 100%;
+  background: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background: #ffffff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import '@/assets/css/base.css'
+import axios from 'axios'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
