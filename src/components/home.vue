a9b2eb5f864b9f052a01f0d34c8520df2fc6e77a
qindan19930701 <837096569@qq.com>
2019-11-17 11:25:25 +0800

    设置主页头部

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,16 @@
 <template>
-<div>
-  home----
-</div>
+<el-container class="container">
+  <el-header><el-row>
+  <el-col :span="4"><img src="../assets/logo.png" alt="图片加载失败"></el-col>
+  <el-col :span="19" class="maidle"><h2>电商后台管理系统</h2></el-col>
+  <el-col :span="1" ><a href="#" class="logout">退出</a></el-col>
+</el-row>
+</el-header>
+  <el-container>
+    <el-aside class="aside" width="200px">Aside</el-aside>
+    <el-main class="main">Main</el-main>
+  </el-container>
+</el-container>
 </template>
 <script>
 export default {
@@ -9,5 +18,23 @@ export default {
 }
 </script>
 <style >
-
+.container{
+background: #b3c0d1;
+height: 100%;
+}
+.aside{
+background: blue;
+}
+.main{
+background: gold;
+}
+.maidle{
+  line-height: 60px;
+  text-align: center;
+  color: #ffffff;
+}
+.logout{
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
