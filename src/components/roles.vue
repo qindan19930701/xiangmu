c820965c3a7b0bc023ff37a92a08f16f05876910
qindan19930701 <837096569@qq.com>
2019-11-19 15:13:55 +0800

    设置面包屑

---
+++ src/components/roles.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card class="box">
+    <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
+    <el-button type="primary" class="btn">添加角色</el-button>
+  </el-card>
+</template>
+<script>
+export default {
+data () {
+return {
+  roles: []
+}
+},
+created () {
+this.getRoles()
+},
+methods:{
+async getRoles() {
+  const res = await this.$http.get(`roles`)
+}
+},
+}
+</script>
+<style>
+.box{
+  height: 100%;
+}
+.btn{
+  margin-top: 20px;
+}
+</style>
