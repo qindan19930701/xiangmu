548556a93a94379aa753f1612b350931ac643d76
qindan19930701 <837096569@qq.com>
2019-11-19 21:26:07 +0800

    展示角色权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -3,7 +3,7 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <el-button type="primary" class="btn">添加角色</el-button>
       <!-- 表格 -->
-    <el-table :data="roles" style="width: 100%">
+    <el-table height="380px" :data="roles" style="width: 100%">
       <el-table-column type="expand"  width="80">
         <template slot-scope="scope">
           <!-- 行列布局 -->
@@ -55,6 +55,7 @@
       :props 配置选中{label/children} -->
           <el-tree
           :data="treelist"
+          default-expand-all
           show-checkbox
           node-key="id"
           :default-expanded-keys="arrExpanded"
@@ -96,13 +97,40 @@ export default {
         this.roles = data
       }
     },
-    async showRight () {
+    async showRight (role) {
       this.dialogFormVisible = true
       const res = await this.$http.get(`rights/tree`)
       console.log(res)
-      const {data,meta:{msg,status}} = res.data
-      if (status===200){
+      const {data, meta: {msg, status}} = res.data
+      console.log(msg)
+      if (status === 200) {
         this.treelist = data
+        // 嵌套遍历取id
+        // const temp = []
+        // this.treelist.forEach(item1 => {
+        //   temp.push(item1.id)
+        //   item1.children.forEach(item2 => {
+        //     temp.push(item2.id)
+        //     item2.children.forEach(item3 => {
+        //       temp.push(item3.id)
+        //     })
+        //   })
+        // })
+        // this.arrExpanded = temp
+        // 获取当前角色的权限id
+        // console.log(role)
+        const temp2 = []
+        role.children.forEach(item1 => {
+          // temp2.push(item1.id)
+          item1.children.forEach(item2 => {
+            // temp2.push(item2.id)
+            item2.children.forEach(item3 => {
+              temp2.push(item3.id)
+            })
+          })
+        })
+        this.arrCheck = temp2
+        // console.log(temp2)
       }
     },
     async deleRight (role, rights) {
