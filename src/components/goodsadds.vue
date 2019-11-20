a05e3bdf397eaccabf91fbeca95b8ee048fe359d
qindan19930701 <837096569@qq.com>
2019-11-20 17:00:58 +0800

    级联使用

--- src/components/goodsadds.vue
+++ src/components/goodsadds.vue
@@ -1,8 +1,8 @@
 <template>
   <el-card class="box">
     <cus-bread level1="商品管理" level2="商品列表"></cus-bread>
-   <el-alert class="alert" title="添加商品信息" type="info" center show-icon> </el-alert>
-     <!-- 步骤条 -->
+    <el-alert class="alert" title="添加商品信息" type="info" center show-icon> </el-alert>
+        <!-- 步骤条 -->
      <el-steps :active="active*1" align-center>
        <el-step title="基本信息" description=""></el-step>
        <el-step title="商品参数" description=""></el-step>
@@ -12,7 +12,7 @@
      </el-steps>
      <!-- 标签页 -->
     <el-form class="box1" label-position="top" label-width="80px" :model="form" >
-     <el-tabs v-model="active" tab-position="left" >
+     <el-tabs v-model="active" tab-position="left" style="height: 200px;">
           <el-tab-pane name="1" label="基本信息">
             <el-form-item label="商品名称">
               <el-input v-model="form.goods_name"></el-input>
@@ -27,15 +27,7 @@
               <el-input v-model="form.goods_number"></el-input>
             </el-form-item>
             <el-form-item label="商品分类">
-              <!-- 级联选择器
-              options 数据来源 -->
-              <el-cascader
-              clearable
-              expand-trigger="hover"
-                 v-model="selectedOptions"
-                 :options="options"
-                 :props="defaultProp"
-                 @change="handleChange"></el-cascader>
+              <el-input v-model="form.goods_cat"></el-input>
             </el-form-item>
           </el-tab-pane>
           <el-tab-pane name="2" label="商品参数"></el-tab-pane>
@@ -50,8 +42,8 @@
 export default {
 data(){
   return{
-   active:"1",
-   form:{
+    active:'1',
+    form:{
       goods_name:'',
       goods_cat:'',
       goods_price	:'',
@@ -60,37 +52,15 @@ data(){
       goods_introduce	:'',
       pics	:'',
       attrs:''
-    },
-    // 级联使用数据
-    options:[],
-    selectedOptions:[],
-    defaultProp:{
-      label:'cat_name',
-      value:'cat_id',
-      children:'children'
     }
   }
-},
-created(){
-this.getGoodsCate()
-},
-methods:{
-  // 获取三级分类
- async getGoodsCate(){
-   const res = await this.$http.get(`categories?type=3`)
-   console.log(res)
-   const {data,meta:{msg,status}} = res.data
-   if (status===200){
-     this.options=data
-   }
- },
-handleChange(){}
 },
 }
 </script>
 <style>
 .box{
   height: 100%;
+  overflow: auto;
 }
 .alert{
   margin-top: 20px;
