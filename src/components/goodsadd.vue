614d5268933e4bc28305f2cb680bbf2d10ba1a26
qindan19930701 <837096569@qq.com>
2019-11-20 20:14:29 +0800

    复选框遍历

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -12,7 +12,7 @@
      </el-steps>
      <!-- 标签页 -->
     <el-form class="box1" label-position="top" label-width="80px" :model="form" >
-     <el-tabs v-model="active" tab-position="left" >
+     <el-tabs v-model="active" tab-position="left" @tab-click="changeTab()">
           <el-tab-pane name="1" label="基本信息">
             <el-form-item label="商品名称">
               <el-input v-model="form.goods_name"></el-input>
@@ -38,7 +38,17 @@
                  @change="handleChange"></el-cascader>
             </el-form-item>
           </el-tab-pane>
-          <el-tab-pane name="2" label="商品参数"></el-tab-pane>
+          <el-tab-pane name="2" label="商品参数">
+            <!-- 复选数组 -->
+             <el-form-item :label="v1.attr_name"
+             v-for="(v1,i) in arrDy" :key="v1.attr_id"
+             >
+              <el-checkbox-group v-model="checkList">
+                    <el-checkbox :label="v2" v-for="(v2,i) in v1.attr_vals" :key="i"></el-checkbox>
+
+             </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
           <el-tab-pane name="3" label="商品属性"></el-tab-pane>
           <el-tab-pane name="4" label="商品图片"></el-tab-pane>
           <el-tab-pane name="5" label="商品内容"></el-tab-pane>
@@ -68,7 +78,9 @@ data(){
       label:'cat_name',
       value:'cat_id',
       children:'children'
-    }
+    },
+    arrDy:[],
+    checkList:[]
   }
 },
 created(){
@@ -78,13 +90,37 @@ methods:{
   // 获取三级分类
  async getGoodsCate(){
    const res = await this.$http.get(`categories?type=3`)
-   console.log(res)
+
    const {data,meta:{msg,status}} = res.data
    if (status===200){
      this.options=data
    }
  },
-handleChange(){}
+handleChange(){},
+async changeTab(){
+  if(this.active==="2"){
+    if(this.selectedOptions.length!==3){
+      this.$message.error("请选择三级分类")
+      return
+    }
+    // 获取动态参数数据
+    const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=many`)
+    const {data,meta:{msg,status}} = res.data
+    if (status===200){
+      this.arrDy=data
+
+      // 此时this.arrDy中的attr_vals是字符串
+      this.arrDy.forEach(v =>{
+
+      if(v.attr_vals.length===0){
+        v.attr_vals= []
+      }else{
+      v.attr_vals=  v.attr_vals.trim().split(",")
+      }
+      })
+    }
+  }
+},
 },
 }
 </script>
